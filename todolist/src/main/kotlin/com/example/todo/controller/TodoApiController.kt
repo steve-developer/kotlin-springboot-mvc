aa284a3fe30b097bde48ab2e3db55176e1a0b010
steve <steve.ye.developer@gmail.com>
2020-09-20 16:00:17 +0900

    Add controller test code

--- todolist/src/main/kotlin/com/example/todo/controller/TodoApiController.kt
+++ todolist/src/main/kotlin/com/example/todo/controller/TodoApiController.kt
@@ -3,6 +3,7 @@ package com.example.todo.controller
 import com.example.todo.model.dto.TodoDto
 import com.example.todo.model.entity.Todo
 import com.example.todo.service.TodoService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -13,8 +14,8 @@ class TodoApiController(
 ) {
 
     @PostMapping("")
-    fun create(@Valid @RequestBody todoDto: TodoDto): Todo {
-        return todoService.create(todoDto)
+    fun create(@Valid @RequestBody todoDto: TodoDto): ResponseEntity<Todo> {
+        return ResponseEntity.ok(todoService.create(todoDto))
     }
 
     @GetMapping("/{index}")
--- todolist/src/test/kotlin/com/example/todo/model/dto/TodoDtoTest.kt
+++ todolist/src/test/kotlin/com/example/todo/model/dto/TodoDtoTest.kt
@@ -2,11 +2,9 @@ package com.example.todo.model.dto
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import javax.validation.Validator
 
 @SpringBootTest
