51b91ed6c620a7edaca98b8e9a03eccf89fb04c1
steve <steve.ye.developer@gmail.com>
2020-09-20 14:52:26 +0900

    Add controller and service

---
+++ todolist/src/main/kotlin/com/example/todo/controller/TodoApiController.kt
@@ -0,0 +1,40 @@
+package com.example.todo.controller
+
+import com.example.todo.model.dto.TodoDto
+import com.example.todo.model.entity.Todo
+import com.example.todo.service.TodoService
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/todo")
+class TodoApiController(
+        val todoService: TodoService
+) {
+
+    @PostMapping("")
+    fun create(@Valid @RequestBody todoDto: TodoDto): Todo {
+        return todoService.create(todoDto)
+    }
+
+    @GetMapping("/{index}")
+    fun read(@PathVariable(required = false) index: Int?): Todo? {
+        return todoService.read(index)
+    }
+
+    @GetMapping("/all")
+    fun all(): MutableList<Todo> {
+        return todoService.all()
+    }
+
+    @PutMapping("")
+    fun update(@Valid @RequestBody todoDto: TodoDto): Todo {
+        return todoService.update(todoDto)
+    }
+
+    @DeleteMapping("")
+    fun delete(@RequestParam(required = true) index: Int?){
+        return todoService.delete(index)
+    }
+
+}
